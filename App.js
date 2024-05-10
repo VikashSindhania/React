39531ff708dc4ce4879eaee6f745ce223742cf57
Vikash <vikashsindhania@gmail.com>
2024-05-10 05:49:10 +0530

    Paet - 04 (A)

--- App.js
+++ App.js
@@ -1,146 +1,109 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-//React.createElement creates an => object for u  => root.render takes and convert it to the html and push it to the browser.
-//React.createElement ==> ReactElement ==> JS Object => HTMLElement(render)
+/**
+ * Header
+ *   Logo
+ *     Nav Items
+ * Body
+ *    Search
+ *      Restaurant Container
+ *        Restaurant Card
+ *       -Img    
+ *        -Name of the Res,Star Rating ,cuisine, 
+ * 
+ * Footer
+ *   Copyright
+ *     Links
+ *      Address
+ *        Contact
+ *
+ 
+ *
+ */
 
-// const heading = React.createElement("h1", { id: "heading" }, "Namaste React");
-// const root = ReactDOM.createRoot(document.getElementById("root"));
-// root.render(heading);
+// Functional Component ==. At the end of the day is a Normal Javascript  Function
+// Component is like a Function.
 
-//JSX - HtM like or XML-like syntax; html + js
-//JSX - JSX is different & React is different
-
-//JSX (transpiled before it reaches the JS) - doing by PARCEL -->
-// --> Babel(compiling the code to understand the browser Understand & our React also Understand)
-
-// JSX Works => Uses React.createElement ==> after that converted it into React Element ==> Which is JS object ==> finally HTMLElement(render) it.
-//Above JSX conversion  is done by Babel at each step Conversion
-//const jsxHeadingOne = <h1 id="heading">Namaste React using JSX </h1>;
-//const root = ReactDOM.createRoot(document.getElementById("root"));
-//root.render(jsxHeadingOne); //in this render we are rendering React Element like this not Functional Component We can render like this.
-//root.render(<HeadingComponent />); // Functional Component render like this
-
-//React Element
-const jsxHeading = (
-  <h1
-    id="heading"
-    className="head" // you have to follow Camel Case
-    tabIndex="5"
-  >
-    Namaste React using JSX
-  </h1>
-);
-
-const fn1 = () => true; // This means that function is returning an true;
-const fn2 = () => {
-  return true;
-}; // This means that function is returning an true;
-
-//React Component
-//Class Based Component  -- Old Way of Writing Code
-//Functional Based Component -- New Way of Writing Code
-
-const HeadingComponent = () => {
-  return <h1 className="heading">Namaste React using Functional Component</h1>;
+const styleCard = {
+  // style Card is an object Here
+  backgroundColor: "#f0f0f0",
 };
-const roots = ReactDOM.createRoot(document.getElementById("root"));
-roots.render(<HeadingComponent />);
 
-const HeadingComponent2 = () => (
-  <h1 className="heading">Namaste React using Functional Component2</h1>
-);
-
-const HeadingComponent3 = () => (
-  <div id="heading">
-    <h1 className="heading">Namaste React using Functional Component2</h1>
-  </div>
-);
-
-//React Functional Component
-//Functional Component -->  A function which is returning an piece of JSX is known as React Functional Component.
-//Functional Component --> is a  normal JavaScript Function that returns React Elememt & piece of JSX Code.
-
-//React Functional Component {
-const HeadingComponent4 = () => (
-  // starting letter should be Capital in case of Functional Component
-  <h1
-    id="heading"
-    className="head" // you have to follow Camel Case
-    tabIndex="5"
-  >
-    Namaste React using JSX
-  </h1>
-);
-//}
-
-//React Element
-// -->
-const HeadingComponent5 = (
-  <h1
-    id="heading"
-    className="head" // you have to follow Camel Case
-    tabIndex="5"
-  >
-    Namaste React using JSX
-  </h1>
-);
-// // <--
-
-//Below code is Known as Component Composition
-const TitleA = () => (
-  <h1 className="heads" tabIndex="5">
-    Namaste React Using JSX....
-  </h1>
-);
-
-const HeadingFunctionalComponent = () => (
-  // Added parentheses here
-  <div id="heading">
-    <Title /> or <Title></Title>
-    <h1 className="heading">Welcome!</h1>
-  </div>
-); // Added semicolon here
-
-const roote = ReactDOM.createRoot(document.getElementById("root"));
-//console.log(HeadingFunctionalComponents);
-
-roote.render(<HeadingFunctionalComponent />);
-
-//Now With React Element
-
-const elem = <span>React Element :)</span>;
-
-const Title = () => (
-  // This title here is behaving as an Functional Component.
-  <h1 className="heads" tabIndex="5">
-    Hello Namaste React Using JSX....
-  </h1>
-);
-
-const title = // This title here is behaving as an React Element.
-  (
-    <h1 className="head" tabIndex="5">
-      {elem}
-      Namaste React Using JSX....
-    </h1>
+const Header = () => {
+  return (
+    <div className="header">
+      <div className="logo-container">
+        <img
+          className="logo"
+          src="https://i.pinimg.com/736x/b0/1e/69/b01e69dfd04399324803c453b0fb9029.jpg"
+          alt="foodLogo"
+        ></img>
+      </div>
+      <div className="nav-items">
+        <ul>
+          <li>Home</li>
+          <li>About Us</li>
+          <li>Contact Us</li>
+          <li>Cart</li>
+        </ul>
+      </div>
+      <div className="footer"></div>
+    </div>
   );
-const number = 1000;
+};
 
-const HeadingFunctionalComponents = () => (
-  // Added parentheses here
-  <div id="container">
-    {number} //Any Javascipt Code you write inside this Curly Braces! And it
-    will comes down as an HTML.
-    {title}
-    {Title()}
-    {100 + 120}
-    {console.log("Javascript code you are Writing Insisde HTML")}
-    <h1 className="heading">Welcome!</h1>
-  </div>
-); // Added semicolon here
+const RestaurantCard = () => {
+  return (
+    <div className="res-card" style={styleCard}>
+      {"All Javascript code inside in these Curly Braces. "}
+      {/* Inline Style ,   we can also write like this style = {{  backgroundColor: "#f0f0f0",}} first Curly Braces there is a Some piece of Javasscript inside it & second curly braces refers an javascript object */}
+      <img
+        className="res-logo"
+        src="https://b.zmtcdn.com/data/pictures/chains/1/50691/ecfdc75d21bd7cd9d880f05c8382bc65.jpg"
+        alt="res-logo"
+      />
+      <h3>Meghana Foods</h3>
+      <h4>Biryani, North Indian , Asian</h4>
+      <h4>4.4 stars</h4>
+      <h4>38 minutes</h4>
+    </div>
+  );
+};
+
+// Props Are just argument to our function
+const Body = () => {
+  return (
+    // return a Piece of JSX code
+    <div className="body">
+      <div className="search">Search</div>
+      <div className="res-container">
+        {/* The Componment which will be always be in Reuse its better to define that functional Component Independently*/}
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+      </div>
+    </div>
+  );
+};
+
+const AppLayout = () => {
+  return (
+    <div className="app">
+      <Header />
+      <Body />
+    </div>
+  );
+};
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-console.log(HeadingFunctionalComponents);
-
-root.render(<HeadingFunctionalComponents />);
+root.render(<AppLayout />);
